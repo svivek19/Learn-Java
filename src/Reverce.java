947a3b7d584f74f85de1f17cc2b143b5c2f1db2c
svivek19 <svivek030503@gmail.com>
2024-12-29 21:42:57 +0530

    reverce a given num

---
+++ src/Reverce.java
@@ -0,0 +1,15 @@
+public class Reverce {
+    public static void main(String[] args) {
+        int num = 42424345;
+
+        int ans = 0;
+
+        while (num > 0){
+            int reminder = num % 10;
+            num /= 10;
+            ans = ans * 10 + reminder;
+        }
+
+        System.out.println(ans);
+    }
+}
