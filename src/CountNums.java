a024d610fcdadd5c07fc7c41c91ce251aa2ea5df
svivek19 <svivek030503@gmail.com>
2024-12-29 21:35:11 +0530

    count 'x' of the given no

--- src/CountNums.java
+++ src/CountNums.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class CountNums {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        int val = in.nextInt();
+
+        int count = 0;
+
+        while (val > 0){
+            int reminder = val % 10;
+            if (reminder == 3) count++;
+            val /= 10;
+        }
+
+        System.out.println("No of counts: " + count );
     }
 }
